7bf52c9ef48de45a4b29eef8ce6b66a209f08e4b
Abhiram M <abhiramm913@gmail.com>
2022-02-25 00:11:46 +0530

    result formatting

--- popup/popup.script.js
+++ popup/popup.script.js
@@ -1,9 +1,8 @@
 const title = document.getElementById("title");
-const meaningElement = document.getElementById("meaning");
 
 const word = localStorage.getItem("currentLookupWord");
 
-title.innerHTML = word;
+title.innerText = word;
 
 const xhr = new XMLHttpRequest();
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word;
@@ -12,6 +11,27 @@ xhr.send();
 
 xhr.onload = function() {
     const response = JSON.parse(xhr.response);
-    const meaning = response[0].meanings[0].definitions[0].definition;
-    meaningElement.innerHTML = meaning;
+    formatResponse(response[0]);
+}
+
+function formatResponse(response) {
+    const phonetic = document.getElementById("phonetic");
+    phonetic.innerText = response.phonetic ? response.phonetic : "";
+    const meanings = response.meanings;
+    const body = document.getElementById("body");
+    for(let meaning of meanings) {
+        const definitions = meaning.definitions;
+        const meaningDivs = document.createElement("div");
+        const partOfSpeechLabel = document.createElement("h3");
+        partOfSpeechLabel.appendChild(document.createTextNode(meaning.partOfSpeech));
+        meaningDivs.appendChild(partOfSpeechLabel);
+        const ul = document.createElement("ul");
+        for(let definition of definitions) {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(definition.definition));
+            ul.appendChild(li);
+        }
+        meaningDivs.appendChild(ul);
+        body.appendChild(meaningDivs);
+    }
 }
